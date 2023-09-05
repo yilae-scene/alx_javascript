e0c857494682d556d36bd38120454ea957471631
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-09-05 12:05:17 +0300

    Print lines

--- 0-javascript_is_amazing.js
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/javascript
-let myVar = 'JavaScript is amazing'
-console.log(myVar)
---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,12 @@
+#! /usr/bin/javascript
+
+
+let firstLine = 'C is fun'
+let secondLine = 'Python is cool'
+let thirdLine = 'JavaScript is amazing'
+
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine)
+
+
