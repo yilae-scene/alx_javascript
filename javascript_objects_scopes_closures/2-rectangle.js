242d2536941715a993de5a5f8c56cb7bc9a6cf9b
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-09-08 19:25:52 +0300

    Add #!

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,14 +1,14 @@
-#!usr/bin/node
+#!/usr/bin/node
 
 class Rectangle {
-  width;
-  height;
-  constructor(w, h) {
-    if (w >= 1 && h >= 1) {
-      this.width = w;
-      this.height = h;
+    width;
+    height;
+    constructor(w, h) {
+        if (w >= 1 && h >= 1) {
+            this.width = w;
+            this.height = h;
+        }
     }
-  }
 }
 
 module.exports = Rectangle;
