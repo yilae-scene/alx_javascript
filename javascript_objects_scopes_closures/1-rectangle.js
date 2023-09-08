57d12685a76d0001c9ef21bc699356913f2268df
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-09-08 19:20:36 +0300

    Check if sides are >=1

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -4,9 +4,14 @@ class Rectangle {
     width;
     height;
     constructor(w, h) {
-        this.width = w;
-        this.height = h;
+        if (w >= 1 && h >= 1) {
+            this.width = w;
+            this.height = h;
+        }
+
     }
 }
 
 module.exports = Rectangle;
+
+
