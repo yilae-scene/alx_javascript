158abb6ff3146cbaa17e73aae2c79a90b1aa9164
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-09-09 12:42:54 +0300

    Update

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,4 +1,4 @@
-//#!/usr/bin/node
+#!/usr/bin/node
 
 class Rectangle {
     //   width;
@@ -11,10 +11,12 @@ class Rectangle {
     }
     print() {
         for (let i = 0; i < this.height; i++) {
-          for (let j = 0; j < this.width; j++) {
-            process.stdout.write("x");
-          }
-          console.log();
+            for (let j = 0; j < this.width; j++) {
+                process.stdout.write("x");
+            }
+            console.log();
         }
     }
 }
+
+module.exports = Rectangle;
