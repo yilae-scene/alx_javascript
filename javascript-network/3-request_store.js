9a9e6776189ffb95d8979cde9cd0a3a0ae9dd76e
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-09-14 20:00:34 +0300

    gsdfgf

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -4,4 +4,4 @@ const requ = require('request')
 const fs = require('fs')
 const url = process.argv[2];
 const path = process.argv[3];
-requ.get(url, {encoding : 'UTF -8'}).pipe(fs.createWriteStream(path))
+requ.get(url).pipe(fs.createWriteStream(path))
