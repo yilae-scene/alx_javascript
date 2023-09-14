eb1c8d388cf577af081b71309de974703d4c0cfd
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-09-14 14:57:19 +0300

    Initial commit

---
+++ javascript-network/0-statuscode.js
@@ -0,0 +1,9 @@
+//#!/usr/bin/node
+
+const request = require('request')
+
+url = process.argv[2]
+request.get(url, function (error, response) {
+    console.log('error: ', error)
+    console.log('code:', response.statusCode);
+})
---
+++ javascript-network/README.md
@@ -0,0 +1,2 @@
+a directory that deals with mainly the workings of javascript network
+
