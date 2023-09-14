70dc6e984d7074fec72b6b555ea249911d4f03e6
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-09-14 19:01:35 +0300

    Print the title

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,10 +1,20 @@
-#!/usr/bin/node
+//#!/usr/bin/node
 
+//const { title } = require("process");
 const request = require("request");
 
 const Id = process.argv[2];
-url = `https://swapi-api.alx-tools.com/api/films/:${Id}`;
-request.get(url, function (error, response) {
-  //console.log('error: ', error)
-  console.log("code:", response.statusCode);
+const url = `https://swapi-api.alx-tools.com/api/films/ ${Id}`;
+request.get(url, function (error, response, body) {
+    if (error){
+    console.log('error: ', error)
+    }
+    else{
+    const objBody = JSON.parse(body)
+        console.log(objBody['title'])
+    }
+    // objBody['results'].forEach(film => {
+    //     console.log(film.title)
+    // })
+    //console.log("code:", response.statusCode);
 });
