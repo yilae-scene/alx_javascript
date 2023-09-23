df15453bb7e65870e0b211e0957de944185ddc90
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-09-23 16:50:42 +0300

    A Digital Clock that is an Chrome extention

--- clockfolder/Content.js
+++ clockfolder/Content.js
@@ -1,29 +1,29 @@
 let weekdays = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednsday",
-    "Thursday",
-    "Friday",
-    "Saturday",
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednsday",
+  "Thursday",
+  "Friday",
+  "Saturday",
 ];
 function ticks() {
-    const dateOutPut = document.getElementById("date");
-    const timeOutPut = document.getElementById("time");
-    const currentDate = new Date();
+  const dateOutPut = document.getElementById("date");
+  const timeOutPut = document.getElementById("time");
+  const currentDate = new Date();
 
-    const year = currentDate.getFullYear();
-    const month = String(currentDate.getMonth() + 1).padStart(2, "0");
-    const date = String(currentDate.getDate()).padStart(2, "0");
-    const day = currentDate.getDay();
-    const dayInStr = weekdays[day];
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+  const date = String(currentDate.getDate()).padStart(2, "0");
+  const day = currentDate.getDay();
+  const dayInStr = weekdays[day];
 
-    const hour = String(currentDate.getHours()).padStart(2, "0");
-    const minute = String(currentDate.getMinutes()).padStart(2, "0");
-    const second = String(currentDate.getSeconds()).padStart(2, "0");
+  const hour = String(currentDate.getHours()).padStart(2, "0");
+  const minute = String(currentDate.getMinutes()).padStart(2, "0");
+  const second = String(currentDate.getSeconds()).padStart(2, "0");
 
-    dateOutPut.innerText = `${year}-${month}-${date} : ${dayInStr}`;
-    timeOutPut.innerHTML = `${hour}: ${minute}: ${second}`;
+  dateOutPut.innerText = `${year}-${month}-${date} : ${dayInStr}`;
+  timeOutPut.innerHTML = `${hour}: ${minute}: ${second}`;
 }
 
 setInterval(ticks, 1000);
